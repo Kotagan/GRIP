9b5b43e246dc18f42bbc8abed136d451d5e2aa6d
kotaka <guodeduan2007@163.com>
2023-12-06 00:23:47 +0900

    fix: bug

--- data_process_origin.py
+++ data_process_origin.py
@@ -41,7 +41,6 @@ def get_origin_data_list(pra_file_path):
     frame_list = np.empty(shape=[0, 10])
     content = np.array(pd.read_csv(pra_file_path, header=None).to_numpy()[:, [1, 0, 2, 20, 21, 22, 52, 54, 56, 50]])
     map_id = {}  # record object id
-    map_list = {}  # record same data
     pair_list = {}  # record same frame + id
     id_count = 1
 
@@ -50,23 +49,18 @@ def get_origin_data_list(pra_file_path):
     converter = Transformer.from_crs(crs_wgs84, crs_jgd2011)
 
     for row in content:
-        # delete same data
-        if str(row) in map_list:
-            continue
-        map_list[str(row)] = 0
-
         # change object_id(str) to object_id(int)
         if not (row[object_id] in map_id):
             map_id[row[object_id]] = id_count
             id_count += 1
         row[object_id] = map_id[row[object_id]]
 
-        data_frame_id = round(row[frame_id] / 500, 0)
+        data_frame_id = int(round(row[frame_id] / 500, 0))
 
         # if pair(frame_id, object_id) exist, continue
         if str(list[data_frame_id, row[object_id]]) in pair_list and compare_which_is_close(pair_list[list(data_frame_id, row[object_id])][frame_id], row[frame_id]):
             continue
-        pair_list[str(list[row[frame_id], row[object_id]])] = row
+        pair_list[str([data_frame_id, row[object_id]])] = row
 
     for index, key in enumerate(pair_list):
         row = pair_list[key]
